3174b95b09d5754508c202784ec48169ba493b17
Subbu-Diras <subbaiahd@nfcsolutionsusa.com>
2026-04-23 19:57:11 +0530

    Updates

--- _next/static/RoikOVdbIceedvsDwqWfe/_buildManifest.js
+++ _next/static/RoikOVdbIceedvsDwqWfe/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/WBM//_next/:path+",
+        "destination": "/WBM/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
